3ec35de913230faae00393f516c1d28ee2e402fc
Jakub Hromadka <jakubhromadkaa@gmail.com>
2022-03-07 13:08:06 +0100

    eshop

---
+++ projekty/eshop/products.js
@@ -0,0 +1,18 @@
+var products = [
+  {
+    id: 1,
+    brand: "nike",
+    name: "Nike Odyssey React Shield",
+    price: 130,
+    img1: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike01a.png",
+    img2: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike01b.png",
+  },
+  {
+    id: 2,
+    brand: "nike",
+    name: "Nike Odyssey React Shield",
+    price: 130,
+    img1: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike01a.png",
+    img2: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike01b.png",
+  },
+];
